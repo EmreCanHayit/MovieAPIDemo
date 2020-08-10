9f5cc5e692785c19d9194fd5e293387e82421698
Emre Can Hayıt <emrecan.hayit@gmail.com>
2020-08-10 12:22:02 +0300

    C:/Program Files/Git/api/directors Post Method, director add

--- routes/movie.js
+++ routes/movie.js
@@ -7,6 +7,7 @@ const Movie = require('../models/Movie');
 // All Movie
 router.get('/', (req, res) => {
   const promise = Movie.find({ });
+  
   promise.then((data) => {
     res.json(data);
   }).catch((err) => {
@@ -17,6 +18,7 @@ router.get('/', (req, res) => {
 // Top 10 Movie
 router.get('/top10', (req, res) => {
   const promise = Movie.find({ }).limit(10).sort({ imdb_score : -1 });
+
   promise.then((data) => {
     res.json(data);
   }).catch((err) => {
@@ -73,6 +75,7 @@ router.delete('/:movie_id', (req, res, next) => {
 router.get('/between/:start_year/:end_year', (req, res) => {
   const { start_year, end_year } = req.params;
   const promise = Movie.find({ year : { "$gte": parseInt(start_year), "$lte": parseInt(end_year) } });
+
   promise.then((data) => {
     res.json(data);
   }).catch((err) => {
