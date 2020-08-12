9d6d99859757026a3380831749a2e6047ab8ae12
Emre Can Hayıt <emrecan.hayit@gmail.com>
2020-08-12 18:38:00 +0300

    api/movies post add movie test script

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -21,16 +21,45 @@ describe('/api/movies tests', () => {
             });
     });
 
-    describe('Movies test', () => {
+    describe('/GET movie', () => {
         it('it should GET all the movie', (done) => {
             chai.request(server)
-            .get('api/movies')
-            .set('x-access-token', token)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-            });
+                .get('api/movies')
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                });
             done();
+        });
+    });
+
+    describe('/POST movie', () => {
+        it('it should POST a movie', (done) => {
+            const movie = {
+                title: 'Test title',
+                director_id: '5f3125169b1d3a2534976339',
+                category: 'Test category',
+                country: 'Test country',
+                year: 2000,
+                imdb_score: 8
+            };
+
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    done();
+                });
         });
     });
 });
